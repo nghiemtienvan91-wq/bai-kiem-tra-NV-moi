8ba9ebcfb0d7309082487b453ece895b61643817
Lens Vietnam <nghiemtienvan91@gmail.com>
2026-09-10 22:32:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://cjgspfyrxupmuoenwtag.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'DÁN_PUBLISHABLE_KEY_CỦA_ANH_VÀO_ĐÂY'
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_8m7Fq1_j-zGmeCPaPIDU_g_UhK2KJel'
 };
